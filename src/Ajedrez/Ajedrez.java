56c28891c5cccc4bccde61ad1ad3bc9988ed303d
Carlos3888 <110266967+Carlos3888@users.noreply.github.com>
2022-08-12 16:04:42 -0200

    Verificacion de movimientos de lo blancos

--- src/Ajedrez/Ajedrez.java
+++ src/Ajedrez/Ajedrez.java
@@ -25,23 +25,47 @@ public class Ajedrez {
         for (int i = 0; i < 8; i++) {
             tablero[1][i] = new Peon('p', i, 1, false);
         }
-        tablero[7][3] = new Dama('q', 3, 7, true);
-        tablero[7][4] = new Rey('k', 4, 7, true);
-        tablero[7][2] = new Arfil('b', 2, 7, true);
-        tablero[7][5] = new Arfil('b', 5, 7, true);
-        tablero[7][1] = new Caballo('n', 1, 7, true);
-        tablero[7][6] = new Caballo('n', 6, 7, true);
-        tablero[7][0] = new Torre('r', 0, 7, true);
-        tablero[7][7] = new Torre('r', 7, 7, true);
+        tablero[7][3] = new Dama('Q', 3, 7, true);
+        tablero[7][4] = new Rey('K', 4, 7, true);
+        tablero[7][2] = new Arfil('B', 2, 7, true);
+        tablero[7][5] = new Arfil('B', 5, 7, true);
+        tablero[7][1] = new Caballo('N', 1, 7, true);
+        tablero[7][6] = new Caballo('N', 6, 7, true);
+        tablero[7][0] = new Torre('R', 0, 7, true);
+        tablero[7][7] = new Torre('R', 7, 7, true);
         for (int i = 0; i < 8; i++) {
-            tablero[6][i] = new Peon('p', i, 6, false);
+            tablero[6][i] = new Peon('P', i, 6, false);
         }
         while(salida == true){
             
             if(turno = true){
+                System.out.print(jugador1+" ingrese el movimiento a realizar: ");
+                String mov = lea.next();
+                String num1 = "";
+                String num2 = "";
+                
+                char p1 = mov.charAt(0);
+                
+                char letra1 = mov.charAt(2);
+                char num_1 = mov.charAt(3);
+                num1 += num_1;
+                int y_1 = Integer.parseInt(num1);
+                int x_1 = posicion(letra1);
+                
+                char letra2 = mov.charAt(5);
+                char num_2 = mov.charAt(6);
+                num2 += num_2;
+                int y_2 = Integer.parseInt(num2);
+                int x_2 = posicion(letra2);
+                
+                if(tablero[y_1][x_1].verifica(mov, tablero)){
+                    
+                    turno = false;
+                }else{
+                    System.out.println("Movimiento incorrecto");
+                }
                 
                 
-                turno = false;
             }else{
                 
                 
@@ -49,6 +73,36 @@ public class Ajedrez {
             }
         }
     }
+    public static int posicion(char letra){
+        int numero = 0;
+        if(letra == 'A'){
+            numero = 0;
+        }
+        if(letra == 'B'){
+            numero = 0;
+        }
+        if(letra == 'C'){
+            numero = 0;
+        }
+        if(letra == 'D'){
+            numero = 0;
+        }
+        if(letra == 'E'){
+            numero = 0;
+        }
+        if(letra == 'F'){
+            numero = 0;
+        }
+        if(letra == 'G'){
+            numero = 0;
+        }
+        if(letra == 'H'){
+            numero = 0;
+        }
+        return numero;
+    }
     
-    
+    public static void Imprimir(){
+        
+    }
 }
