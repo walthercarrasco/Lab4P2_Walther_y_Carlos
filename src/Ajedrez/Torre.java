b8a43a9bc47b71060b254d421ce36ceb2f213f03
Carlos3888 <110266967+Carlos3888@users.noreply.github.com>
2022-08-12 17:24:41 -0200

    Actualizacion

--- src/Ajedrez/Torre.java
+++ src/Ajedrez/Torre.java
@@ -12,8 +12,25 @@ public class Torre extends Piezas{
     }
 
     @Override
-    public boolean verifica(String mov, Piezas[][] tablero, int x, int y, int x2, int y2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean verifica(String mov, Piezas[][] tablero, int x, int y, int x2, int y2, boolean turno) {
+         boolean temporal = true;
+        char l1 = mov.charAt(0);
+        char l2 = mov.charAt(1);
+        char l3 = mov.charAt(4);
+        if((l1 == 'r' || l1 == 'R') && tablero[y][x] instanceof Torre){
+            if(l2 == '|' && l3 == '-'){
+                if(x == x2){
+                    while(y != 12){
+
+                    }
+                }
+            }else{
+                temporal = false;
+            }
+        }else{
+            temporal = false;
+        }
+        return temporal;
     }    
     
 }
