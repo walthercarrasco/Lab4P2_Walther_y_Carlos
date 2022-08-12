9f34d8cf6491e63b6fd91dfe0b741ecb8f0bebdc
Carlos3888 <110266967+Carlos3888@users.noreply.github.com>
2022-08-12 14:08:09 -0200

    creacion del tablero

--- src/Ajedrez/Piezas.java
+++ src/Ajedrez/Piezas.java
@@ -2,6 +2,24 @@ package Ajedrez;
 
 public class Piezas {
     private String[][] tablero = new String[9][9];
+
+    public String[][] getTablero() {
+        return tablero;
+    }
+
+    public void setTablero(String[][] tablero) {
+        this.tablero = tablero;
+    }
+
+    public Piezas() {
+    }
+
+    
+    public Piezas(String [][] tablero) {
+        this.tablero = tablero;
+    }
+    
+    
     
     public boolean movimiento(int x_i, int y_i, int x_m, int y_m, String[][] tablero){
         boolean temporal = true;
