251670050d80f4589b45256d96eeb53277a9fa83
Carlos3888 <110266967+Carlos3888@users.noreply.github.com>
2022-08-12 15:22:18 -0200

    Creacion del tablero inicial

--- src/Ajedrez/Ajedrez.java
+++ src/Ajedrez/Ajedrez.java
@@ -11,8 +11,42 @@ public class Ajedrez {
         System.out.println("Jugador #2: ");
         String jugador2 = lea.next();
         boolean salida = true;
+        boolean turno = true;
+        
+        Piezas[][] tablero = new Piezas[8][8];
+        tablero[0][3] = new Dama('q', 3, 0, false);
+        tablero[0][4] = new Rey('k', 4, 0, false);
+        tablero[0][2] = new Arfil('b', 2, 0, false);
+        tablero[0][5] = new Arfil('b', 5, 0, false);
+        tablero[0][1] = new Caballo('n', 1, 0, false);
+        tablero[0][6] = new Caballo('n', 6, 0, false);
+        tablero[0][0] = new Torre('r', 0, 0, false);
+        tablero[0][7] = new Torre('r', 7, 0, false);
+        for (int i = 0; i < 8; i++) {
+            tablero[1][i] = new Peon('p', i, 1, false);
+        }
+        tablero[7][3] = new Dama('q', 3, 7, true);
+        tablero[7][4] = new Rey('k', 4, 7, true);
+        tablero[7][2] = new Arfil('b', 2, 7, true);
+        tablero[7][5] = new Arfil('b', 5, 7, true);
+        tablero[7][1] = new Caballo('n', 1, 7, true);
+        tablero[7][6] = new Caballo('n', 6, 7, true);
+        tablero[7][0] = new Torre('r', 0, 7, true);
+        tablero[7][7] = new Torre('r', 7, 7, true);
+        for (int i = 0; i < 8; i++) {
+            tablero[6][i] = new Peon('p', i, 6, false);
+        }
         while(salida == true){
-            salida = false;
+            
+            if(turno = true){
+                
+                
+                turno = false;
+            }else{
+                
+                
+                turno = true;
+            }
         }
     }
     
