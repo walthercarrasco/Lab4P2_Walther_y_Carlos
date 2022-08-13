2b427164c793ffe6a9e87f68eaa737f1a981661c
Walther Carrasco <110209326+walthercarrasco@users.noreply.github.com>
2022-08-12 19:39:53 -0600

    Ya no se que poner XD

--- src/Ajedrez/Peon.java
+++ src/Ajedrez/Peon.java
@@ -19,13 +19,9 @@ public class Peon extends Piezas{
         char l2 = mov.charAt(1);
         char l3 = mov.charAt(4);
         if((l1 == 'p' || l1 == 'P') && tablero[y][x] instanceof Peon){
-            System.out.println("1");
-            if(l2 == '|' && l3 == '-' ){
-                System.out.println("2");
+            if(l2 == '|' && l3 == '-'){
                 if(turno == true){
-                    System.out.println("3");
                     if(turno != tablero[y][x].isColor()){
-                        System.out.println("4");
                         if((tablero[y-1][x] == null && y-1 == y2 && x == x2) || (tablero[y][x].isColor() != tablero[y2][x2].isColor() && y-1 == y2 && x-1 == x2) || (tablero[y][x].isColor() != tablero[y2][x2].isColor() && y-1 == y2 && x+1 == x2)){
                             System.out.println("5");
                             return true;
@@ -35,8 +31,9 @@ public class Peon extends Piezas{
                     }else{
                         return false;
                     }
+                    
                 }else if(turno == false){
-                    if(turno != tablero[y][x].isColor()){
+                    if(turno == tablero[y][x].isColor()){
                         if((tablero[y+1][x] == null && y+1 == y2 && x == x2) || (tablero[y][x].isColor() != tablero[y2][x2].isColor() && y+1 == y2 && x-1 == x2) || (tablero[y][x].isColor() != tablero[y2][x2].isColor() && y+1 == y2 && x+1 == x2)){
                             return true;
                         }else{
